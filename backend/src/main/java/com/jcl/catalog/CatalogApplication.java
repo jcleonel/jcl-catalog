9ed159ac0448f7754ef9a1e13a76e410aecf9574
jcleonel <jean.leonel.costa@gmail.com>
2021-09-21 14:39:28 -0300

    First upload test in AWS S3

--- backend/src/main/java/com/jcl/catalog/CatalogApplication.java
+++ backend/src/main/java/com/jcl/catalog/CatalogApplication.java
@@ -1,13 +1,27 @@
 package com.jcl.catalog;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CatalogApplication {
+import com.jcl.catalog.services.S3Service;
 
+@SpringBootApplication
+public class CatalogApplication implements CommandLineRunner {
+
+	@Autowired
+	private S3Service s3Service; 	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CatalogApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		s3Service.uploadFile("D:\\img\\print.jpg");
+		
 	}
 
 }
